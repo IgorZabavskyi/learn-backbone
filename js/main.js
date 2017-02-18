27f8e5646437c77c8d68b3d9cd708fc03db96a1e
Igor Zabavsky <finboss@gmail.com>
2017-02-18 14:09:18 +0200

    CSS & JS directories was added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Ig on 02/17/2017.
+ */
+var Person = function (config){
+    this.name = config.name;
+    this.age = config.age;
+    this.job = config.job;
+};
+
+var nick = new Person({name: 'Nick', age: 24, job: 'Developer'});
